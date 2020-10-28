715815fa223cd788a03fb2e337c67c6d1791968b
Lingfei Kong <colin404@foxmail.com>
2020-10-28 12:14:33 +0800

    refactor(middleware): add requestid to logger

--- internal/pkg/middleware/logger.go
+++ internal/pkg/middleware/logger.go
@@ -7,7 +7,6 @@ package middleware
 import (
 	"fmt"
 	"io"
-	"net/http"
 	"os"
 	"time"
 
@@ -16,122 +15,8 @@ import (
 	"github.com/mattn/go-isatty"
 )
 
-type consoleColorModeValue int
-
-const (
-	autoColor consoleColorModeValue = iota
-	disableColor
-	forceColor
-)
-
-const (
-	green   = "\033[97;42m"
-	white   = "\033[90;47m"
-	yellow  = "\033[90;43m"
-	red     = "\033[97;41m"
-	blue    = "\033[97;44m"
-	magenta = "\033[97;45m"
-	cyan    = "\033[97;46m"
-	reset   = "\033[0m"
-)
-
-var consoleColorMode = autoColor
-
-// LoggerConfig defines the config for Logger middleware.
-type LoggerConfig struct {
-	// Optional. Default value is gin.defaultLogFormatter
-	Formatter LogFormatter
-
-	// Output is a writer where logs are written.
-	// Optional. Default value is gin.DefaultWriter.
-	Output io.Writer
-
-	// SkipPaths is a url path array which logs are not written.
-	// Optional.
-	SkipPaths []string
-}
-
-// LogFormatter gives the signature of the formatter function passed to LoggerWithFormatter.
-type LogFormatter func(params LogFormatterParams) string
-
-// LogFormatterParams is the structure any formatter will be handed when time to log comes.
-type LogFormatterParams struct {
-	Request *http.Request
-
-	// TimeStamp shows the time after the server returns a response.
-	TimeStamp time.Time
-	// StatusCode is HTTP response code.
-	StatusCode int
-	// Latency is how much time the server cost to process a certain request.
-	Latency time.Duration
-	// ClientIP equals Context's ClientIP method.
-	ClientIP string
-	// Method is the HTTP method given to the request.
-	Method string
-	// Path is a path the client requests.
-	Path string
-	// ErrorMessage is set if error has occurred in processing the request.
-	ErrorMessage string
-	// isTerm shows whether does gin's output descriptor refers to a terminal.
-	isTerm bool
-	// BodySize is the size of the Response Body
-	BodySize int
-	// Keys are the keys set on the request's context.
-	Keys map[string]interface{}
-}
-
-// StatusCodeColor is the ANSI color for appropriately logging http status code to a terminal.
-func (p *LogFormatterParams) StatusCodeColor() string {
-	code := p.StatusCode
-
-	switch {
-	case code >= http.StatusOK && code < http.StatusMultipleChoices:
-		return green
-	case code >= http.StatusMultipleChoices && code < http.StatusBadRequest:
-		return white
-	case code >= http.StatusBadRequest && code < http.StatusInternalServerError:
-		return yellow
-	default:
-		return red
-	}
-}
-
-// MethodColor is the ANSI color for appropriately logging http method to a terminal.
-func (p *LogFormatterParams) MethodColor() string {
-	method := p.Method
-
-	switch method {
-	case http.MethodGet:
-		return blue
-	case http.MethodPost:
-		return cyan
-	case http.MethodPut:
-		return yellow
-	case http.MethodDelete:
-		return red
-	case http.MethodPatch:
-		return green
-	case http.MethodHead:
-		return magenta
-	case http.MethodOptions:
-		return white
-	default:
-		return reset
-	}
-}
-
-// ResetColor resets all escape attributes.
-func (p *LogFormatterParams) ResetColor() string {
-	return reset
-}
-
-// IsOutputColor indicates whether can colors be outputted to the log.
-func (p *LogFormatterParams) IsOutputColor() bool {
-	return consoleColorMode == forceColor || (consoleColorMode == autoColor && p.isTerm)
-}
-
 // defaultLogFormatter is the default log format function Logger middleware uses.
-var defaultLogFormatter = func(param LogFormatterParams) string {
+var defaultLogFormatter = func(param gin.LogFormatterParams) string {
 	var statusColor, methodColor, resetColor string
 	if param.IsOutputColor() {
 		statusColor = param.StatusCodeColor()
@@ -143,52 +28,26 @@ var defaultLogFormatter = func(param LogFormatterParams) string {
 		// Truncate in a golang < 1.8 safe way
 		param.Latency = param.Latency - param.Latency%time.Second
 	}
-	return fmt.Sprintf("%s %3d %s| %13v | %15s |%s %-7s %s %#v\n%s",
+	return fmt.Sprintf("%s%3d%s - [%s] \"%v %s%s%s %s\" %s",
 		//param.TimeStamp.Format("2006/01/02 - 15:04:05"),
 		statusColor, param.StatusCode, resetColor,
-		param.Latency,
 		param.ClientIP,
+		param.Latency,
 		methodColor, param.Method, resetColor,
 		param.Path,
 		param.ErrorMessage,
 	)
 }
 
-// DisableConsoleColor disables color output in the console.
-func DisableConsoleColor() {
-	consoleColorMode = disableColor
-}
-
-// ForceConsoleColor force color output in the console.
-func ForceConsoleColor() {
-	consoleColorMode = forceColor
-}
-
-// ErrorLogger returns a handlerfunc for any error type.
-func ErrorLogger() gin.HandlerFunc {
-	return ErrorLoggerT(gin.ErrorTypeAny)
-}
-
-// ErrorLoggerT returns a handlerfunc for a given error type.
-func ErrorLoggerT(typ gin.ErrorType) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.Next()
-		errors := c.Errors.ByType(typ)
-		if len(errors) > 0 {
-			c.JSON(-1, errors)
-		}
-	}
-}
-
 // Logger instances a Logger middleware that will write the logs to gin.DefaultWriter.
 // By default gin.DefaultWriter = os.Stdout.
 func Logger() gin.HandlerFunc {
-	return LoggerWithConfig(LoggerConfig{})
+	return LoggerWithConfig(GetLoggerConfig(nil, nil, nil))
 }
 
 // LoggerWithFormatter instance a Logger middleware with the specified log format function.
-func LoggerWithFormatter(f LogFormatter) gin.HandlerFunc {
-	return LoggerWithConfig(LoggerConfig{
+func LoggerWithFormatter(f gin.LogFormatter) gin.HandlerFunc {
+	return LoggerWithConfig(gin.LoggerConfig{
 		Formatter: f,
 	})
 }
@@ -196,14 +55,14 @@ func LoggerWithFormatter(f LogFormatter) gin.HandlerFunc {
 // LoggerWithWriter instance a Logger middleware with the specified writer buffer.
 // Example: os.Stdout, a file opened in write mode, a socket...
 func LoggerWithWriter(out io.Writer, notlogged ...string) gin.HandlerFunc {
-	return LoggerWithConfig(LoggerConfig{
+	return LoggerWithConfig(gin.LoggerConfig{
 		Output:    out,
 		SkipPaths: notlogged,
 	})
 }
 
 // LoggerWithConfig instance a Logger middleware with config.
-func LoggerWithConfig(conf LoggerConfig) gin.HandlerFunc {
+func LoggerWithConfig(conf gin.LoggerConfig) gin.HandlerFunc {
 	formatter := conf.Formatter
 	if formatter == nil {
 		formatter = defaultLogFormatter
@@ -221,6 +80,10 @@ func LoggerWithConfig(conf LoggerConfig) gin.HandlerFunc {
 	if w, ok := out.(*os.File); !ok || os.Getenv("TERM") == "dumb" ||
 		(!isatty.IsTerminal(w.Fd()) && !isatty.IsCygwinTerminal(w.Fd())) {
 		isTerm = false
+	}
+
+	if isTerm {
+		gin.ForceConsoleColor()
 	}
 
 	var skip map[string]struct{}
@@ -244,9 +107,8 @@ func LoggerWithConfig(conf LoggerConfig) gin.HandlerFunc {
 
 		// Log only when path is not being skipped
 		if _, ok := skip[path]; !ok {
-			param := LogFormatterParams{
+			param := gin.LogFormatterParams{
 				Request: c.Request,
-				isTerm:  isTerm,
 				Keys:    c.Keys,
 			}
 
--- internal/pkg/middleware/requestid.go
+++ internal/pkg/middleware/requestid.go
@@ -1,29 +1,88 @@
-// Copyright 2020 Lingfei Kong <colin404@foxmail.com>. All rights reserved.
-// Use of this source code is governed by a MIT style
-// license that can be found in the LICENSE file.
-
 package middleware
 
 import (
+	"fmt"
+	"io"
+	"time"
+
 	"github.com/gin-gonic/gin"
 	uuid "github.com/satori/go.uuid"
 )
 
-const headerXRequestID = "X-Request-ID"
+const (
+	XRequestIDKey = "X-Request-ID"
+)
 
-// RequestID add X-Request-ID to response header.
+//RequestID is a middleware that injects a 'X-Request-ID' into the context and request/response header of each request.
 func RequestID() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		// Check for incoming header, use it if exists
-		rid := c.GetHeader(headerXRequestID)
+		rid := c.GetHeader(XRequestIDKey)
 
 		if rid == "" {
 			rid = uuid.Must(uuid.NewV4()).String()
-			c.Header(headerXRequestID, rid)
+			c.Request.Header.Set(XRequestIDKey, rid)
+			c.Set(XRequestIDKey, rid)
 		}
 
-		// Set headerXRequestID header
-		c.Writer.Header().Set(headerXRequestID, rid)
+		// Set XRequestIDKey header
+		c.Writer.Header().Set(XRequestIDKey, rid)
 		c.Next()
 	}
+}
+
+// GetLoggerConfig return gin.LoggerConfig which will write the logs to specified io.Writer with given gin.LogFormatter.
+// By default gin.DefaultWriter = os.Stdout
+// reference: https://github.com/gin-gonic/gin#custom-log-format
+func GetLoggerConfig(formatter gin.LogFormatter, output io.Writer, skipPaths []string) gin.LoggerConfig {
+	if formatter == nil {
+		formatter = GetDefaultLogFormatterWithRequestID()
+	}
+	return gin.LoggerConfig{
+		Formatter: formatter,
+		Output:    output,
+		SkipPaths: skipPaths,
+	}
+}
+
+//GetDefaultLogFormatterWithRequestID returns gin.LogFormatter with 'RequestID'
+func GetDefaultLogFormatterWithRequestID() gin.LogFormatter {
+	return func(param gin.LogFormatterParams) string {
+		var statusColor, methodColor, resetColor string
+		if param.IsOutputColor() {
+			statusColor = param.StatusCodeColor()
+			methodColor = param.MethodColor()
+			resetColor = param.ResetColor()
+		}
+
+		if param.Latency > time.Minute {
+			// Truncate in a golang < 1.8 safe way
+			param.Latency = param.Latency - param.Latency%time.Second
+		}
+		return fmt.Sprintf("%s%3d%s [%s] - [%s] \"%v %s%s%s %s\" %s",
+			//param.TimeStamp.Format("2006/01/02 - 15:04:05"),
+			statusColor, param.StatusCode, resetColor,
+			param.Request.Header.Get(XRequestIDKey),
+			param.ClientIP,
+			param.Latency,
+			methodColor, param.Method, resetColor,
+			param.Path,
+			param.ErrorMessage,
+		)
+	}
+}
+
+// GetRequestIDFromContext returns 'RequestID' from the given context if present.
+func GetRequestIDFromContext(c *gin.Context) string {
+	if v, ok := c.Get(XRequestIDKey); ok {
+		if requestID, ok := v.(string); ok {
+			return requestID
+		}
+	}
+	return ""
+}
+
+// GetRequestIDFromHeaders returns 'RequestID' from the headers if present.
+func GetRequestIDFromHeaders(c *gin.Context) string {
+	return c.Request.Header.Get(XRequestIDKey)
 }
