758c8a5d0eadaa46bf8eb7b0e190c4124ab92326
Lingfei Kong <colin404@foxmail.com>
2020-10-31 15:17:35 +0800

    test: test sample code for jwt

---
+++ test/jwt/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/dgrijalva/jwt-go/v4"
+)
+
+func main() {
+	rawJWT := `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJpYW0uYXV0aHoubWFybW90ZWR1LmNvbSIsImV4cCI6MTYwNDEyODQwMywiaWF0IjoxNjA0MTI4NDAyLCJpc3MiOiJpYW1jdGwiLCJraWQiOiJpZDEifQ.Itr5u4C-nTeA01qbjjl7RzuPD-aSQazsJZY_Z25aGnI`
+
+	// Verify the token
+	claims := &jwt.MapClaims{}
+	parsedT, err := jwt.ParseWithClaims(rawJWT, claims, func(token *jwt.Token) (interface{}, error) {
+		// Validate the alg is HMAC signature
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+
+		if kid, ok := token.Header["kid"].(string); ok {
+			fmt.Println("kid", kid)
+		}
+
+		return []byte("key1"), nil
+	}, jwt.WithAudience("iam.authz.marmotedu.com"))
+
+	if err != nil || !parsedT.Valid {
+		fmt.Println("token valid failed", err)
+		return
+	}
+
+	fmt.Println("ok")
+}
