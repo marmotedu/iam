e184784a4303b368db5be6746c8efe2e4b031613
Lingfei Kong <colin404@foxmail.com>
2021-03-02 15:27:21 +0800

    chore: add release.tag to push latest tag to github

--- scripts/make-rules/release.mk
+++ scripts/make-rules/release.mk
@@ -8,12 +8,16 @@
 #
 
 .PHONY: release.run
-release.run: release.verify release.tag
+release.run: release.verify release.ensure-tag
 	@scripts/release.sh
 
 .PHONY: release.verify
 release.verify: tools.verify.git-chglog tools.verify.github-release tools.verify.coscmd
 
 .PHONY: release.tag
-release.tag: tools.verify.gsemver
+release.tag: tools.verify.gsemver release.ensure-tag
+	@git push origin v`gsemver bump`
+
+.PHONY: release.ensure-tag
+release.ensure-tag: tools.verify.gsemver
 	@scripts/ensure_tag.sh
