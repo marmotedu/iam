534d249b7e0c0c545ce7e3fe203df9848c1468c6
Lingfei Kong <lkong@tencent.com>
2021-05-28 18:43:30 +0800

    docs(changelog): add CHANGELOG-1.0.0.md

--- scripts/force_release.sh
+++ scripts/force_release.sh
@@ -2,23 +2,5 @@
 
 IAM_ROOT=$(dirname "${BASH_SOURCE[0]}")/..    
 source "${IAM_ROOT}/scripts/lib/init.sh"
-
-if [ $# -ne 1 ];then
-  iam::log::error "Usage: force_release.sh v1.0.0"
-  exit 1  
-fi
-
-version="$1"
-
-# 1. delete old version
-git tag -d ${version}
-git push origin --delete ${version}
-
-# 2. create a new tag
-git tag -a ${version} -m "release ${version}"
-git push origin master
-git push origin ${version}
-
-# 3. release the new release
 pushd ${IAM_ROOT}
 make release
