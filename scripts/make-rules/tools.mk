9253a97d3227ea703958972c486df76594db33e6
Lingfei Kong <lkong@tencent.com>
2021-07-07 20:40:53 +0800

    fix: fix `make tools` error

--- scripts/make-rules/tools.mk
+++ scripts/make-rules/tools.mk
@@ -6,9 +6,12 @@
 # Makefile helper functions for tools
 #
 
-TOOLS ?=$(BLOCKER_TOOLS) $(CRITICAL_TOOLS) $(TRIVIAL_TOOLS))
+TOOLS ?=$(BLOCKER_TOOLS) $(CRITICAL_TOOLS) $(TRIVIAL_TOOLS)
 
+.PHONY: tools.install
 tools.install: $(addprefix tools.install., $(TOOLS))
+
+.PHONY: tools.install.%
 tools.install.%:
 	@echo "===========> Installing $*"
 	@$(MAKE) install.$*
