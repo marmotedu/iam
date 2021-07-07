b2a9d457860814b5db0271dba04fce8de44755e0
Lingfei Kong <lkong@tencent.com>
2021-07-07 20:51:05 +0800

    chore: add V module log example

--- pkg/log/example/context/main.go
+++ pkg/log/example/context/main.go
@@ -44,26 +44,16 @@ func main() {
 	log.Init(opts)
 	defer log.Flush()
 
-	// Debug、Info(with field)、Warnf、Errorw使用
-	log.Debug("This is a debug message")
-	log.Info("This is a info message", log.Int32("int_key", 10))
-	log.Warnf("This is a formatted %s message", "warn")
-	log.Errorw("Message printed with Errorw", "X-Request-ID", "fbf54504-64da-4088-9b86-67824a7fb508")
-
 	// WithValues使用
 	lv := log.WithValues("X-Request-ID", "7a7b9f24-4cae-4b2a-9464-69088b45b904")
-	lv.Infow("Info message printed with [WithValues] logger")
-	lv.Infow("Debug message printed with [WithValues] logger")
 
 	// Context使用
+	lv.Infof("Start to call pirntString function")
 	ctx := lv.WithContext(context.Background())
+	pirntString(ctx, "World")
+}
+
+func pirntString(ctx context.Context, str string) {
 	lc := log.FromContext(ctx)
-	lc.Info("Message printed with [WithContext] logger")
-
-	ln := lv.WithName("test")
-	ln.Info("Message printed with [WithName] logger")
-
-	// V level使用
-	log.V(1).Info("This is a V level message")
-	log.V(1).Infow("This is a V level message with fields", "X-Request-ID", "7a7b9f24-4cae-4b2a-9464-69088b45b904")
+	lc.Infof("Hello %s", str)
 }
