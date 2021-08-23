6c6c574d2df403e8c58c5f3b726021de5019e2b5
Lingfei Kong <lkong@tencent.com>
2021-08-24 02:48:04 +0800

    chore: add missing copyright header

--- build/docker/iamctl/build.sh
+++ build/docker/iamctl/build.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# Copyright 2020 Lingfei Kong <colin404@foxmail.com>. All rights reserved.
+# Use of this source code is governed by a MIT style
+# license that can be found in the LICENSE file.
+
+
 mkdir -p "$DST_DIR/scripts/install"
 
 cp -rv scripts/lib "$DST_DIR/scripts/"
