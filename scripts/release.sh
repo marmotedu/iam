53efd5cfe54790f59f4c176927dfa1083ce90f5a
Lingfei Kong <lkong@tencent.com>
2021-09-09 10:19:21 +0800

    docs(changelog): add CHANGELOG-1.1.0.md

--- scripts/release.sh
+++ scripts/release.sh
@@ -21,8 +21,8 @@ iam::golang::setup_env
 iam::build::verify_prereqs
 iam::release::verify_prereqs
 #iam::build::build_image
-#iam::build::build_command
-#iam::release::package_tarballs
-#iam::release::updload_tarballs
-#iam::release::github_release
+iam::build::build_command
+iam::release::package_tarballs
+iam::release::updload_tarballs
+iam::release::github_release
 iam::release::generate_changelog
