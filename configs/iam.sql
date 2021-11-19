f870baf28ddd48ad2c73472d19f1a58ccefc60a4
Lingfei Kong <colin404@foxmail.com>
2021-11-19 20:12:28 +0800

    fix(watcher): add missing fields in user table

--- configs/iam.sql
+++ configs/iam.sql
@@ -150,12 +150,14 @@ CREATE TABLE `user` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `instanceID` varchar(20) DEFAULT NULL,
   `name` varchar(45) NOT NULL,
+  `status` int(1) DEFAULT 1 COMMENT '1:可用，0:不可用',
   `nickname` varchar(30) NOT NULL,
   `password` varchar(255) NOT NULL,
   `email` varchar(256) NOT NULL,
   `phone` varchar(20) DEFAULT NULL,
   `isAdmin` tinyint(1) unsigned NOT NULL DEFAULT 0 COMMENT '1: administrator\\\\n0: non-administrator',
   `extendShadow` longtext DEFAULT NULL,
+  `loginedAt` timestamp NULL DEFAULT NULL COMMENT 'last login time',
   `createdAt` timestamp NOT NULL DEFAULT current_timestamp(),
   `updatedAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`),
@@ -170,7 +172,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (0,'user-lingfei','admin','admin','$2a$10$WnQD2DCfWVhlGmkQ8pdLkesIGPf9KJB7N1mhSOqulbgN7ZMo44Mv2','admin@foxmail.com','1812884xxxx',1,'{}','2021-05-27 10:01:40','2021-05-05 21:13:14');
+INSERT INTO `user` VALUES (1,'user-lingfei','admin',1,'admin','$2a$10$WnQD2DCfWVhlGmkQ8pdLkesIGPf9KJB7N1mhSOqulbgN7ZMo44Mv2','admin@foxmail.com','1812884xxxx',1,'{}',now(),'2021-05-27 10:01:40','2021-05-05 21:13:14');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
