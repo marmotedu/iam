89f6e0c9ea3ac47a397906e24ab3573c2c79f03d
Lingfei Kong <lkong@tencent.com>
2022-06-16 21:13:08 +0800

    chore: optimize version generate rule

--- scripts/ensure_tag.sh
+++ scripts/ensure_tag.sh
@@ -4,7 +4,11 @@
 # Use of this source code is governed by a MIT style
 # license that can be found in the LICENSE file.
 
-version=v`gsemver bump`
-if [ -z "`git tag -l $version`" ];then
-  git tag -a -m "release version $version" $version
+version="${VERSION}"
+if [ "${version}" == "" ];then
+  version=v`gsemver bump`
+fi
+
+if [ -z "`git tag -l ${version}`" ];then
+  git tag -a -m "release version ${version}" ${version}
 fi
